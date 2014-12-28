678263ff8de5199003e7a2edeb58e94fb2e0ef31
nextscript <stavros.d@hotmail.de>
2014-12-28 15:18:51 +0100

    TS3DNS-v.1.0

--- footer.inc.php
+++ footer.inc.php
@@ -3,9 +3,9 @@
  <div style="position:relative; top:-5px;"  class="container-fluid">
   <div class="row">
 	
-	<div class="col-xs-2"></div>
-	<div class="col-xs-8"><div class="navbar navbar-inverse" style="text-align: center; padding: 0px;  border-radius: 0px;" role="navigation"><br><font color="#FFFFFF"><b>Copyright (c) 2014 TS3DNS</font></div></div>
-	<div class="col-xs-2"></div>
+	<div class="col-xs-3"></div>
+	<div class="col-xs-6"><div class="navbar navbar-inverse" style="text-align: center; padding: 0px;  border-radius: 0px;" role="navigation"><br><font color="#FFFFFF"><b>Copyright (c) 2014 TS3DNS</font></div></div>
+	<div class="col-xs-3"></div>
 
   </div>
 </div>
